ef354387d034e151c8df19c40b0d031f1f6ee3cc
Florian Schmitz <FlorianSchmitz1202@gmail.com>
2020-10-20 13:30:18 +0200

    created Bootstrap server config and endpoints

--- portfolio/README.md
+++ portfolio/README.md
@@ -3,6 +3,8 @@
 ### Resources
 [IBMStockTrader Java EE Portfolio Implementation](https://github.com/IBMStockTrader/portfolio/blob/master/src/main/java/com/ibm/hybrid/cloud/sample/stocktrader/portfolio/json/Portfolio.java)
 
+[Vlingo docs](https://docs.vlingo.io/)
+
 ### Data Structure
 	public class Portfolio {
 		private String owner;
--- portfolio/src/main/java/com/whz/portfolio/resource/PortfolioResource.java
+++ portfolio/src/main/java/com/whz/portfolio/resource/PortfolioResource.java
@@ -1,24 +1,47 @@
 package com.whz.portfolio.resource;
 
 import io.vlingo.actors.Stage;
+import io.vlingo.common.Completes;
+import io.vlingo.http.Response;
 import io.vlingo.http.resource.Resource;
 import io.vlingo.http.resource.ResourceHandler;
-import static io.vlingo.http.resource.ResourceBuilder.resource;
 
+import static io.vlingo.http.Response.Status.Ok;
+import static io.vlingo.http.resource.ResourceBuilder.get;
+import static io.vlingo.http.resource.ResourceBuilder.resource;
 
 public class PortfolioResource extends ResourceHandler {
 
-  private final Stage stage;
+	private final Stage stage;
 
-  public PortfolioResource(final Stage stage) {
-      this.stage = stage;
-  }
+	public PortfolioResource(final Stage stage) {
+		this.stage = stage;
+	}
 
+	public Completes<Response> create() {
+		return Completes.withSuccess(Response.of(Ok, "Created"));
+	}
 
-  @Override
-  public Resource<?> routes() {
-     return resource("PortfolioResource" /*Add Request Handlers here as a second parameter*/);
-  }
+	public Completes<Response> retrieve(String id) {
+		return Completes.withSuccess(Response.of(Ok, "Retrieved: " + id));
+	}
 
+	public Completes<Response> update() {
+		return Completes.withSuccess(Response.of(Ok, "Updated"));
+	}
+
+	public Completes<Response> delete() {
+		return Completes.withSuccess(Response.of(Ok, "Deleted"));
+	}
+
+	@Override
+	public Resource<?> routes() {
+		return resource("PortfolioResource", //
+				get("/create").handle(this::create), //
+				get("/retrieve/{id}").param(String.class).handle(this::retrieve), //
+				get("/update").handle(this::update), //
+				get("/delete").handle(this::delete) //
+		);
+	}
 
 }
