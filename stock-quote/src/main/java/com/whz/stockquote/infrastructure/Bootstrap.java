1684418f038f6cb3b80d705b9c43aa879ad43af1
Simon Eckert <simon.eckert.gcb@fh-zwickau.de>
2020-10-26 18:40:38 +0100

    changed default port to 18083

--- stock-quote/src/main/java/com/whz/stockquote/infrastructure/Bootstrap.java
+++ stock-quote/src/main/java/com/whz/stockquote/infrastructure/Bootstrap.java
@@ -17,64 +17,64 @@ import io.vlingo.http.resource.Resources;
 import io.vlingo.http.resource.Server;
 
 public class Bootstrap {
-  private static Bootstrap instance;
-  private static int DefaultPort = 18080;
+    private static Bootstrap instance;
+    private static int DefaultPort = 18083;
 
-  private final Server server;
-  private final World world;
+    private final Server server;
+    private final World world;
 
-  public Bootstrap(final int port) throws Exception {
-    world = World.startWithDefaults("stockquote");
+    public Bootstrap(final int port) throws Exception {
+        world = World.startWithDefaults("stockquote");
 
-    final Stage stage =
-            world.stageNamed("stockquote", Stage.class, new GridAddressFactory(IdentityGeneratorType.RANDOM));
+        final Stage stage =
+                world.stageNamed("stockquote", Stage.class, new GridAddressFactory(IdentityGeneratorType.RANDOM));
 
-    final SourcedTypeRegistry sourcedTypeRegistry = new SourcedTypeRegistry(world);
-    final StatefulTypeRegistry statefulTypeRegistry = new StatefulTypeRegistry(world);
-    QueryModelStateStoreProvider.using(stage, statefulTypeRegistry);
-    CommandModelJournalProvider.using(stage, sourcedTypeRegistry, ProjectionDispatcherProvider.using(stage).storeDispatcher);
+        final SourcedTypeRegistry sourcedTypeRegistry = new SourcedTypeRegistry(world);
+        final StatefulTypeRegistry statefulTypeRegistry = new StatefulTypeRegistry(world);
+        QueryModelStateStoreProvider.using(stage, statefulTypeRegistry);
+        CommandModelJournalProvider.using(stage, sourcedTypeRegistry, ProjectionDispatcherProvider.using(stage).storeDispatcher);
 
-    final StockQuoteResource stockQuoteResource = new StockQuoteResource(stage);
+        final StockQuoteResource stockQuoteResource = new StockQuoteResource(stage);
 
-    Resources allResources = Resources.are(
-        stockQuoteResource.routes()
-    );
+        Resources allResources = Resources.are(
+                stockQuoteResource.routes()
+        );
 
-    server = Server.startWith(stage, allResources, port, Sizing.define(), Timing.define());
+        server = Server.startWith(stage, allResources, port, Sizing.define(), Timing.define());
 
-    Runtime.getRuntime().addShutdownHook(new Thread(() -> {
-      if (instance != null) {
+        Runtime.getRuntime().addShutdownHook(new Thread(() -> {
+            if (instance != null) {
+                instance.server.stop();
+
+                System.out.println("\n");
+                System.out.println("=========================");
+                System.out.println("Stopping stockquote.");
+                System.out.println("=========================");
+            }
+        }));
+    }
+
+    void stopServer() throws Exception {
+        if (instance == null) {
+            throw new IllegalStateException("Schemata server not running");
+        }
         instance.server.stop();
-
-        System.out.println("\n");
-        System.out.println("=========================");
-        System.out.println("Stopping stockquote.");
-        System.out.println("=========================");
-      }
-    }));
-  }
-
-  void stopServer() throws Exception {
-    if (instance == null) {
-      throw new IllegalStateException("Schemata server not running");
-    }
-    instance.server.stop();
-  }
-
-  public static void main(final String[] args) throws Exception {
-    System.out.println("=========================");
-    System.out.println("service: stockquote.");
-    System.out.println("=========================");
-
-    int port;
-
-    try {
-      port = Integer.parseInt(args[0]);
-    } catch (Exception e) {
-      port = DefaultPort;
-      System.out.println("stockquote: Command line does not provide a valid port; defaulting to: " + port);
     }
 
-    instance = new Bootstrap(port);
-  }
+    public static void main(final String[] args) throws Exception {
+        System.out.println("=========================");
+        System.out.println("service: stockquote.");
+        System.out.println("=========================");
+
+        int port;
+
+        try {
+            port = Integer.parseInt(args[0]);
+        } catch (Exception e) {
+            port = DefaultPort;
+            System.out.println("stockquote: Command line does not provide a valid port; defaulting to: " + port);
+        }
+
+        instance = new Bootstrap(port);
+    }
 }
