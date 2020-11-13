a7d141bfc56897b0fcd1f0bd9143e351183c689f
Florian Schmitz <FlorianSchmitz1202@gmail.com>
2020-11-13 20:18:11 +0100

    removed target and .docker folder

--- portfolio/src/main/java/com/whz/portfolio/model/portfolio/PortfolioEntity.java
+++ portfolio/src/main/java/com/whz/portfolio/model/portfolio/PortfolioEntity.java
@@ -1,13 +1,22 @@
 package com.whz.portfolio.model.portfolio;
 
-import java.util.Collections;
 import java.util.List;
+import java.util.Optional;
+
+import com.whz.portfolio.infrastructure.persistence.CommandModelJournalProvider;
 
 import io.vlingo.common.Completes;
+import io.vlingo.common.Outcome;
 import io.vlingo.lattice.model.sourcing.EventSourced;
+import io.vlingo.symbio.Metadata;
+import io.vlingo.symbio.Source;
+import io.vlingo.symbio.store.Result;
+import io.vlingo.symbio.store.StorageException;
+import io.vlingo.symbio.store.journal.Journal.AppendResultInterest;
 
 public final class PortfolioEntity extends EventSourced implements Portfolio {
 	private PortfolioState state;
+	private int version = 0;
 
 	public PortfolioEntity(final String id) {
 		super(id);
@@ -15,6 +24,7 @@ public final class PortfolioEntity extends EventSourced implements Portfolio {
 	}
 
 	public Completes<PortfolioState> portfolioCreated(final String value) {
+		
 		return apply(new PortfolioCreated(state.id, value), () -> state);
 	}
 
@@ -28,6 +38,24 @@ public final class PortfolioEntity extends EventSourced implements Portfolio {
 	}
 
 	private void applyPortfolioCreated(final PortfolioCreated e) {
+		CommandModelJournalProvider.instance().journal.append("PortfolioCreated", version++, e, new AppendResultInterest() {
+			@Override
+			public <S, ST> void appendResultedIn(Outcome<StorageException, Result> outcome, String streamName,
+					int streamVersion, Source<S> source, Metadata metadata, Optional<ST> snapshot, Object object) {
+			}
+			@Override
+			public <S, ST> void appendResultedIn(Outcome<StorageException, Result> outcome, String streamName,
+					int streamVersion, Source<S> source, Optional<ST> snapshot, Object object) {
+			}
+			@Override
+			public <S, ST> void appendAllResultedIn(Outcome<StorageException, Result> outcome, String streamName,
+					int streamVersion, List<Source<S>> sources, Metadata metadata, Optional<ST> snapshot, Object object) {
+			}
+			@Override
+			public <S, ST> void appendAllResultedIn(Outcome<StorageException, Result> outcome, String streamName,
+					int streamVersion, List<Source<S>> sources, Optional<ST> snapshot, Object object) {
+			}
+		}, this);
 		state = state.withPlaceholderValue(e.owner);
 	}
 }
