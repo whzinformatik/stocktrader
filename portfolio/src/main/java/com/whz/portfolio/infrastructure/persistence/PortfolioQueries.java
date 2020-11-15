21aec872e11dfb39647b98c24c7f05dce12be0a0
Florian <47524615+Florian122@users.noreply.github.com>
2020-11-15 14:36:09 +0100

    Create Portfolio (#21)
    * init project
    
    * removed some eclipse files
    
    * added db settings
    
    * added .project files from eclipse to gitignore
    
    * removed eclipse files
    
    * added more eclipse files to gitignore
    
    * added readme
    
    * first build
    
    * added infos to the readme
    
    * created Bootstrap server config and endpoints
    
    * added flyway config
    
    * modified api, added create POST method
    
    * testing..
    
    * refactoring
    
    * fixed some errors
    
    * Changed the sql migration script to create the portfolio table
    
    * added stock quotes rest api
    
    * :construction_worker: remove codeql pipeline
    
    * Changed QuotesCache to enum singleton, source.getClass() fix
    
    * Added spotless, added StockQuotePublisherDemo
    
    * fixed pom
    
    * Removed flyway, added fix for table creation
    
    * changed gitignore
    
    * :art: execute spotless apply
    
    * removed target and .docker folder
    
    * removed 2. gitignore, updated readme in portfolio, refactoring
    
    * :art: execute spotless apply
    
    * remove target folder
    
    * remove flyway dependency
    
    * rename portfolio variable
    
    * remove sysouts
    
    * update java version for portfolio pipeline
    
    * remove sysouts + use logger
    
    * rename method to withOwner
    
    * add little improvements
    
    * upgrade java version
    
    * removed testing code from PortfolioEntity class
    
    Co-authored-by: flaxel <falk.puschner@arcor.de>

---
+++ portfolio/src/main/java/com/whz/portfolio/infrastructure/persistence/PortfolioQueries.java
@@ -0,0 +1,12 @@
+package com.whz.portfolio.infrastructure.persistence;
+
+import com.whz.portfolio.infrastructure.PortfolioData;
+import io.vlingo.common.Completes;
+import java.util.Collection;
+
+public interface PortfolioQueries {
+
+  Completes<PortfolioData> portfolioOf(String id);
+
+  Completes<Collection<PortfolioData>> portfolios();
+}
