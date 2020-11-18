ba6921c6b2fc3754ff683eca427cbc9200c14965
flaxel <falk.puschner@arcor.de>
2020-11-18 22:31:49 +0100

    :white_check_mark: add post feedback test

--- feedback/src/test/java/com/whz/feedback/resource/FeedbackResourceTest.java
+++ feedback/src/test/java/com/whz/feedback/resource/FeedbackResourceTest.java
@@ -1,5 +1,9 @@
 package com.whz.feedback.resource;
 
+import static org.hamcrest.Matchers.*;
+
+import com.whz.feedback.infrastructure.FeedbackData;
+import io.restassured.response.ValidatableResponse;
 import org.junit.jupiter.api.Test;
 
 public class FeedbackResourceTest extends ResourceTestCase {
@@ -7,5 +11,19 @@ public class FeedbackResourceTest extends ResourceTestCase {
   @Test
   public void testReady() {
     givenJsonClient().when().get("/ready").then().statusCode(200);
+  }
+
+  @Test
+  public void testPost() {
+    FeedbackData data = FeedbackData.just("message");
+    ValidatableResponse response =
+        givenJsonClient()
+            .body(data)
+            .when()
+            .post("/")
+            .then()
+            .statusCode(201)
+            .body("id", notNullValue())
+            .body("message", equalTo("message"));
   }
 }
