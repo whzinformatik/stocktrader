27c343f8b6a73e6e0245b33cfbfbb0ed7f7bfba8
flaxel <falk.puschner@arcor.de>
2020-11-18 22:39:20 +0100

    :art: formatting test

--- feedback/src/test/java/com/whz/feedback/resource/FeedbackResourceTest.java
+++ feedback/src/test/java/com/whz/feedback/resource/FeedbackResourceTest.java
@@ -3,7 +3,6 @@ package com.whz.feedback.resource;
 import static org.hamcrest.Matchers.*;
 
 import com.whz.feedback.infrastructure.FeedbackData;
-import io.restassured.response.ValidatableResponse;
 import org.junit.jupiter.api.Test;
 
 public class FeedbackResourceTest extends ResourceTestCase {
@@ -16,14 +15,13 @@ public class FeedbackResourceTest extends ResourceTestCase {
   @Test
   public void testPost() {
     FeedbackData data = FeedbackData.just("message");
-    ValidatableResponse response =
-        givenJsonClient()
-            .body(data)
-            .when()
-            .post("/")
-            .then()
-            .statusCode(201)
-            .body("id", notNullValue())
-            .body("message", equalTo("message"));
+    givenJsonClient()
+        .body(data)
+        .when()
+        .post("/")
+        .then()
+        .statusCode(201)
+        .body("id", notNullValue())
+        .body("message", equalTo("message"));
   }
 }
