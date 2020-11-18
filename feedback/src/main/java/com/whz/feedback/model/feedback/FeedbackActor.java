59217a4ccf133e0941d930b312b21f0a03e17d59
flaxel <falk.puschner@arcor.de>
2020-11-18 21:42:38 +0100

    :sparkles: create rabbitmq publisher

--- feedback/src/main/java/com/whz/feedback/model/feedback/FeedbackActor.java
+++ feedback/src/main/java/com/whz/feedback/model/feedback/FeedbackActor.java
@@ -1,15 +1,29 @@
 package com.whz.feedback.model.feedback;
 
+import com.whz.feedback.resource.Publisher;
 import io.vlingo.common.Completes;
 import io.vlingo.lattice.model.sourcing.EventSourced;
+import java.util.Optional;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public final class FeedbackActor extends EventSourced implements Feedback {
+
+  private final Logger logger = LoggerFactory.getLogger(FeedbackActor.class);
+
+  private static final String EXCHANGE_NAME = "feedback";
+
+  private final Publisher<FeedbackState> publisher;
 
   private FeedbackState state;
 
   public FeedbackActor(final String id) {
     super(id);
     this.state = FeedbackState.identifiedBy(id);
+
+    final String serviceName =
+        Optional.ofNullable(System.getenv("RABBITMQ_SERVICE")).orElse("localhost");
+    this.publisher = new Publisher<>(serviceName);
   }
 
   @Override
@@ -28,5 +42,11 @@ public final class FeedbackActor extends EventSourced implements Feedback {
 
   private void applyFeedbackMessage(final FeedbackSubmittedEvent e) {
     state = state.withMessage(e.message);
+
+    try {
+      publisher.send(EXCHANGE_NAME, state);
+    } catch (Exception ex) {
+      logger.error("cannot publish message", ex);
+    }
   }
 }
