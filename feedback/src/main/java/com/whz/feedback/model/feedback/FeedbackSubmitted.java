f47b76c53be9babd59056be511ede3996a78d8de
flaxel <falk.puschner@arcor.de>
2020-11-20 17:16:41 +0100

    :truck: update resource files

--- feedback/src/main/java/com/whz/feedback/model/feedback/FeedbackSubmitted.java
+++ feedback/src/main/java/com/whz/feedback/model/feedback/FeedbackSubmitted.java
@@ -2,12 +2,12 @@ package com.whz.feedback.model.feedback;
 
 import io.vlingo.lattice.model.IdentifiedDomainEvent;
 
-public final class FeedbackSubmittedEvent extends IdentifiedDomainEvent {
+public final class FeedbackSubmitted extends IdentifiedDomainEvent {
 
   public final String id;
   public final String message;
 
-  public FeedbackSubmittedEvent(final String id, final String message) {
+  public FeedbackSubmitted(final String id, final String message) {
     this.id = id;
     this.message = message;
   }
