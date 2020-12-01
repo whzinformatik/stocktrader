1cb922d64e0bce0a9b6493a0d1716428682a150a
Simon Eckert <simon.eckert.gcb@fh-zwickau.de>
2020-12-01 20:42:26 +0100

    Change unicode and apply spotless

--- stock-quote/src/main/java/com/whz/stockquote/Main.java
+++ stock-quote/src/main/java/com/whz/stockquote/Main.java
@@ -6,15 +6,19 @@ import java.util.List;
 
 public class Main {
 
-    public static void main(String[] args) throws IOException, InterruptedException {
-        String serviceName = System.getenv("RABBITMQ_SERVICE");
-        String exchangeName = System.getenv("RABBITMQ_EXCHANGE");
-        String exchangeType = System.getenv("RABBITMQ_EXCHANGE_TYPE");
+  public static void main(String[] args) throws IOException, InterruptedException {
+    String serviceName = System.getenv("RABBITMQ_SERVICE");
+    String exchangeName = System.getenv("RABBITMQ_EXCHANGE");
+    String exchangeType = System.getenv("RABBITMQ_EXCHANGE_TYPE");
 
-        List<String> symbols = Arrays.asList("TSLA", "INTC", "GOOG", "MSFT", "AMZN", "NVDA", "NFLX", "SBUX", "SAP", "ADDYY", "POAHF", "SAP.DE");
+    List<String> symbols =
+        Arrays.asList(
+            "TSLA", "INTC", "GOOG", "MSFT", "AMZN", "NVDA", "NFLX", "SBUX", "SAP", "ADDYY", "POAHF",
+            "SAP.DE");
 
-        StockQuotePublisher stockQuotePublisher = new StockQuotePublisher(serviceName, exchangeName, exchangeType);
+    StockQuotePublisher stockQuotePublisher =
+        new StockQuotePublisher(serviceName, exchangeName, exchangeType);
 
-        stockQuotePublisher.run(symbols);
-    }
+    stockQuotePublisher.run(symbols);
+  }
 }
--- stock-quote/src/main/java/com/whz/stockquote/StockQuotePublisher.java
+++ stock-quote/src/main/java/com/whz/stockquote/StockQuotePublisher.java
@@ -3,66 +3,69 @@ package com.whz.stockquote;
 import com.rabbitmq.client.Channel;
 import com.rabbitmq.client.Connection;
 import com.rabbitmq.client.ConnectionFactory;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.util.List;
+import java.util.concurrent.TimeoutException;
+import java.util.logging.Logger;
 import org.json.JSONObject;
 import yahoofinance.Stock;
 import yahoofinance.YahooFinance;
 
-import java.io.IOException;
-import java.util.List;
-import java.util.concurrent.TimeoutException;
-import java.util.logging.Logger;
-
 public class StockQuotePublisher {
 
-    private static final Logger LOGGER = Logger.getLogger(StockQuotePublisher.class.getName());
+  private static final Logger LOGGER = Logger.getLogger(StockQuotePublisher.class.getName());
 
-    private final String exchangeName;
-    private final String exchangeType;
+  private final String exchangeName;
+  private final String exchangeType;
 
-    private final ConnectionFactory connectionFactory;
+  private final ConnectionFactory connectionFactory;
 
-    public StockQuotePublisher(String serviceName, String exchangeName, String exchangeType) {
-        this.exchangeName = exchangeName;
-        this.exchangeType = exchangeType;
+  public StockQuotePublisher(String serviceName, String exchangeName, String exchangeType) {
+    this.exchangeName = exchangeName;
+    this.exchangeType = exchangeType;
 
-        this.connectionFactory = new ConnectionFactory();
-        this.connectionFactory.setHost(serviceName);
+    this.connectionFactory = new ConnectionFactory();
+    this.connectionFactory.setHost(serviceName);
+  }
+
+  public void publish(List<String> symbols) throws IOException {
+    try (Connection connection = connectionFactory.newConnection();
+        Channel channel = connection.createChannel()) {
+      channel.exchangeDeclare(exchangeName, exchangeType);
+      for (String s : symbols) {
+        Stock answer = YahooFinance.get(s);
+        String answerJsonString =
+            new JSONObject()
+                .put("symbol", answer.getSymbol())
+                .put("longName", answer.getName())
+                .put("regularMarketPrice", answer.getQuote().getPrice())
+                .put("financialCurrency", answer.getCurrency())
+                .put("regularMarketTime", answer.getQuote().getLastTradeTime().getTime().getTime())
+                .toString();
+        channel.basicPublish(
+            exchangeName, "", null, answerJsonString.getBytes(StandardCharsets.UTF_8));
+      }
+    } catch (TimeoutException e) {
+      e.printStackTrace();
     }
+  }
 
-    public void publish(List<String> symbols) throws IOException {
-        try (Connection connection = connectionFactory.newConnection();
-             Channel channel = connection.createChannel()) {
-            channel.exchangeDeclare(exchangeName, exchangeType);
-            for (String s : symbols) {
-                Stock answer = YahooFinance.get(s);
-                String answerJsonString = new JSONObject()
-                        .put("symbol", answer.getSymbol())
-                        .put("longName", answer.getName())
-                        .put("regularMarketPrice", answer.getQuote().getPrice())
-                        .put("financialCurrency", answer.getCurrency())
-                        .put("regularMarketTime", answer.getQuote().getLastTradeTime().getTime().getTime())
-                        .toString();
-                channel.basicPublish(exchangeName, "", null, answerJsonString.getBytes("UTF-8"));
-            }
-        } catch (TimeoutException e) {
-            e.printStackTrace();
-        }
+  public void run(List<String> symbols) throws IOException, InterruptedException {
+    String randomPublishIntervalString = System.getenv("SET_RANDOM_PUBLISH_INTERVAL");
+
+    if (Boolean.parseBoolean(randomPublishIntervalString)) {
+      while (true) {
+        publish(symbols);
+        Thread.sleep((long) (Math.random() * 3000));
+      }
+    } else {
+      LOGGER.warning(
+          "Environment variable 'SET_RANDOM_PUBLISH_INTERVAL' either incorrect or not set!");
+      while (true) {
+        publish(symbols);
+        Thread.sleep(30000);
+      }
     }
-
-    public void run(List<String> symbols) throws IOException, InterruptedException {
-        String randomPublishIntervalString = System.getenv("SET_RANDOM_PUBLISH_INTERVAL");
-
-        if (Boolean.parseBoolean(randomPublishIntervalString)) {
-            while (true) {
-                publish(symbols);
-                Thread.sleep((long) (Math.random() * 3000));
-            }
-        } else {
-            LOGGER.warning("Environment variable 'SET_RANDOM_PUBLISH_INTERVAL' either incorrect or not set!");
-            while (true) {
-                publish(symbols);
-                Thread.sleep(30000);
-            }
-        }
-    }
+  }
 }
