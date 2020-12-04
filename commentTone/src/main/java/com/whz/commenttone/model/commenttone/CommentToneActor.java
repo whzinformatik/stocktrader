460f2c02ea4e1bef2d10e5e0055235f220f6efbf
kb1prb13 <dastan.namazov.96@gmail.com>
2020-12-04 17:06:44 +0100

    Added rabbitmq publisher and receiver

--- commentTone/src/main/java/com/whz/commenttone/model/commenttone/CommentToneActor.java
+++ commentTone/src/main/java/com/whz/commenttone/model/commenttone/CommentToneActor.java
@@ -1,25 +1,19 @@
 package com.whz.commenttone.model.commenttone;
 
+import com.whz.commenttone.resource.rabbitmq.CommentToneConsumer;
 import com.whz.commenttone.resource.rabbitmq.CommentTonePublisher;
 import io.vlingo.common.Completes;
 import io.vlingo.lattice.model.sourcing.EventSourced;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 import java.util.Optional;
 
 public final class CommentToneActor extends EventSourced implements CommentTone {
 
-    private final Logger logger = LoggerFactory.getLogger(CommentToneActor.class);
-
-    private static final String SEND_EXCHANGE_NAME = "commentTone";
-    private static final String SEND_QUEUE_NAME = "commentTone";
-
-//    private static final String CONSUME_EXCHANGE_NAME = "feedback";
-//    private static final String CONSUME_QUEUE_NAME = "feedback";
-
     private final CommentTonePublisher<CommentToneState> publisher;
-//    private final CommentToneConsumer consumer;
+    private static final String SEND_EXCHANGE_NAME = "commentTone";
+
+    private final CommentToneConsumer consumer;
+    private static final String CONSUME_EXCHANGE_NAME = "feedback";
 
     private CommentToneState state;
 
@@ -32,7 +26,7 @@ public final class CommentToneActor extends EventSourced implements CommentTone 
                 .orElse("localhost");
 
         this.publisher = new CommentTonePublisher<>(serviceName);
-//        this.consumer = new CommentToneConsumer(serviceName);
+        this.consumer = new CommentToneConsumer(serviceName);
     }
 
     @Override
@@ -52,14 +46,10 @@ public final class CommentToneActor extends EventSourced implements CommentTone 
     }
 
     private void applyCommentToneMessage(final CommentTonePublishedEvent e) {
-        logger.info("trying send comment: " + e.toString());
         state = state.withMessage(e.message);
 
-        try {
-//            consumer.consume(CONSUME_EXCHANGE_NAME, CONSUME_QUEUE_NAME);
-            publisher.send(SEND_EXCHANGE_NAME, SEND_QUEUE_NAME, state);
-        } catch (Exception exception) {
-            logger.error("cannot publish message", exception);
-        }
+//            consumer.consume(CONSUME_EXCHANGE_NAME);
+//            publisher.send(SEND_EXCHANGE_NAME, state);
+
     }
 }
