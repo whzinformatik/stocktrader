6195a7f8931c44bf879ca770fefdca87ee353f66
kb1prb13 <dastan.namazov.96@gmail.com>
2020-12-04 17:04:39 +0100

    Added rabbitmq subscriber and receiver

--- commentTone/src/main/java/com/whz/commenttone/resource/rabbitmq/CommentToneConsumer.java
+++ commentTone/src/main/java/com/whz/commenttone/resource/rabbitmq/CommentToneConsumer.java
@@ -1,7 +1,8 @@
 package com.whz.commenttone.resource.rabbitmq;
 
 import com.rabbitmq.client.*;
-import io.vlingo.actors.Logger;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.io.IOException;
 import java.nio.charset.StandardCharsets;
@@ -11,34 +12,39 @@ public class CommentToneConsumer {
 
     private final ConnectionFactory connectionFactory;
 
-    private final Logger logger = Logger.basicLogger();
+    private final Logger logger = LoggerFactory.getLogger(CommentToneConsumer.class);
 
     public CommentToneConsumer(String host) {
         this.connectionFactory = new ConnectionFactory();
         this.connectionFactory.setHost(host);
     }
 
-    public void consume(String exchangeName, String queueName) throws IOException, TimeoutException {
+    public void consume(String exchangeName) {
 
         logger.info("receiving feedback...");
 
         try (final Connection connection = connectionFactory.newConnection();
              final Channel channel = connection.createChannel()) {
 
-            channel.exchangeDeclare(exchangeName, queueName);
+            channel.exchangeDeclare(exchangeName, "fanout");
+
+            String queueName = channel.queueDeclare().getQueue();
+
+            logger.info("queueName is " + queueName);
 
             channel.queueBind(queueName, exchangeName, "");
 
             final Consumer consumer = new DefaultConsumer(channel) {
                 @Override
-                public void handleDelivery(String consumerTag, Envelope envelope,
-                                           AMQP.BasicProperties properties, byte[] body) throws IOException {
+                public void handleDelivery(String consumerTag, Envelope envelope, AMQP.BasicProperties properties, byte[] body) {
                     String message = new String(body, StandardCharsets.UTF_8);
-                    logger.info("received '" + message + "'");
+                    logger.info("received feedback: " + message);
                 }
             };
 
             channel.basicConsume(queueName, true, consumer);
+        } catch (IOException | TimeoutException ex) {
+            logger.info("can't receive feedback: " + ex.getMessage());
         }
     }
 }
--- commentTone/src/main/java/com/whz/commenttone/resource/rabbitmq/CommentTonePublisher.java
+++ commentTone/src/main/java/com/whz/commenttone/resource/rabbitmq/CommentTonePublisher.java
@@ -4,30 +4,29 @@ import com.rabbitmq.client.BuiltinExchangeType;
 import com.rabbitmq.client.Channel;
 import com.rabbitmq.client.Connection;
 import com.rabbitmq.client.ConnectionFactory;
-import io.vlingo.actors.Logger;
 import io.vlingo.common.serialization.JsonSerialization;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
-import java.io.IOException;
 import java.nio.charset.StandardCharsets;
-import java.util.concurrent.TimeoutException;
 
 public class CommentTonePublisher<T> {
 
     private final ConnectionFactory connectionFactory;
 
-    private final Logger logger = Logger.basicLogger();
+    private final Logger logger = LoggerFactory.getLogger(CommentTonePublisher.class);
 
     public CommentTonePublisher(String host) {
         this.connectionFactory = new ConnectionFactory();
         this.connectionFactory.setHost(host);
     }
 
-    public void send(String exchangeName, String queueName, T message) throws IOException, TimeoutException {
-
-        logger.info("sending comment...");
+    public void send(String exchangeName, T message) {
 
         try (final Connection connection = connectionFactory.newConnection();
              final Channel channel = connection.createChannel()) {
+
+            logger.info("sending comment: " + message.toString());
 
             channel.exchangeDeclare(exchangeName, BuiltinExchangeType.TOPIC);
 
