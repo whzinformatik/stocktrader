c4e133f2f1ff1d4e834529a4093ccd6b18d4870e
Lation <timon_schwalbe@web.de>
2020-12-08 14:03:45 +0100

    fixed minor error, applied spotless

--- account/src/main/java/com/whz/account/resource/AccountResource.java
+++ account/src/main/java/com/whz/account/resource/AccountResource.java
@@ -24,7 +24,6 @@ import com.whz.account.infrastructure.persistence.AccountQueries;
 import com.whz.account.infrastructure.persistence.QueryModelStateStoreProvider;
 import com.whz.account.model.account.Account;
 import com.whz.account.model.account.AccountEntity;
-
 import io.vlingo.actors.Stage;
 import io.vlingo.common.Completes;
 import io.vlingo.http.Response;
@@ -33,53 +32,63 @@ import io.vlingo.http.resource.ResourceHandler;
 
 public class AccountResource extends ResourceHandler {
 
-	private final Stage stage;
-	private final AccountQueries accountQueries;
+  private final Stage stage;
+  private final AccountQueries accountQueries;
 
-	public AccountResource(final Stage stage) {
-		this.stage = stage;
-		this.accountQueries = QueryModelStateStoreProvider.instance().accountQueries;
-	}
+  public AccountResource(final Stage stage) {
+    this.stage = stage;
+    this.accountQueries = QueryModelStateStoreProvider.instance().accountQueries;
+  }
 
-	// GET
-	public Completes<Response> handleGetAccount(String id) {
-		return accountQueries.accountOf(id)
-				.andThenTo(AccountData.empty(),
-						data -> Completes.withSuccess(
-								Response.of(Ok, headers(of(ContentType, "application/json")), serialized(data))))
-				.otherwise(noData -> Response.of(NotFound, location(id)));
-	}
+  // GET
+  public Completes<Response> handleGetAccount(String id) {
+    return accountQueries
+        .accountOf(id)
+        .andThenTo(
+            AccountData.empty(),
+            data ->
+                Completes.withSuccess(
+                    Response.of(
+                        Ok, headers(of(ContentType, "application/json")), serialized(data))))
+        .otherwise(noData -> Response.of(NotFound, location(id)));
+  }
 
-	// POST
-	public Completes<Response> handlePostAccount(AccountData data) {
-		return Account.defineWith(stage, data.balance)
-				.andThenTo(state -> Completes.withSuccess(Response.of(Created,
-						headers(of(Location, location(state.id))).and(of(ContentType, "application/json")),
-						serialized(AccountData.from(state)))));
-	}
+  // POST
+  public Completes<Response> handlePostAccount(AccountData data) {
+    return Account.defineWith(stage, data.balance)
+        .andThenTo(
+            state ->
+                Completes.withSuccess(
+                    Response.of(
+                        Created,
+                        headers(of(Location, location(state.id)))
+                            .and(of(ContentType, "application/json")),
+                        serialized(AccountData.from(state)))));
+  }
 
-	/*
-	 * // PUT public Completes<Response> handlePut(AccountData data) { return
-	 * Completes.withSuccess(Response.of(Ok, "Updated")); }
-	 * 
-	 * // DELETE public Completes<Response> handleDelete(String id) { return
-	 * Completes.withSuccess(Response.of(Ok, "Deleted")); }
-	 */
+  /*
+   * // PUT public Completes<Response> handlePut(AccountData data) { return
+   * Completes.withSuccess(Response.of(Ok, "Updated")); }
+   *
+   * // DELETE public Completes<Response> handleDelete(String id) { return
+   * Completes.withSuccess(Response.of(Ok, "Deleted")); }
+   */
 
-	@Override
-	public Resource<?> routes() {
-		return resource(getClass().getSimpleName(),
-				get("/account/{id}").param(String.class).handle(this::handleGetAccount),
-				post("/account").body(AccountData.class).handle(this::handlePostAccount));
-		// put("/account").body(AccountData.class).handle(this::handlePut),
-		// delete("/account/{id}").param(String.class).handle(this::handleDelete));
-	}
+  @Override
+  public Resource<?> routes() {
+    return resource(
+        getClass().getSimpleName(),
+        get("/account/{id}").param(String.class).handle(this::handleGetAccount),
+        post("/account").body(AccountData.class).handle(this::handlePostAccount));
+    // put("/account").body(AccountData.class).handle(this::handlePut),
+    // delete("/account/{id}").param(String.class).handle(this::handleDelete));
+  }
 
-	private String location(final String id) {
-		return "/account/" + id;
-	}
+  private String location(final String id) {
+    return "/account/" + id;
+  }
 
-	private Completes<Account> resolve(final String id) {
-		return stage.actorOf(Account.class, stage.addressFactory().from(id), AccountEntity.class);
-	}
+  private Completes<Account> resolve(final String id) {
+    return stage.actorOf(Account.class, stage.addressFactory().from(id), AccountEntity.class);
+  }
 }
--- account/src/main/java/com/whz/account/resource/StockAcquiredSubscriber.java
+++ account/src/main/java/com/whz/account/resource/StockAcquiredSubscriber.java
@@ -1,48 +1,44 @@
 package com.whz.account.resource;
 
-import java.io.IOException;
-import java.nio.charset.StandardCharsets;
-import java.util.concurrent.TimeoutException;
-
 import com.rabbitmq.client.Channel;
 import com.rabbitmq.client.Connection;
 import com.rabbitmq.client.ConnectionFactory;
 import com.rabbitmq.client.DeliverCallback;
-
-// import com.whz.portfolio.infrastructure.StockQuoteData;
 import io.vlingo.actors.Logger;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.util.concurrent.TimeoutException;
 
 public enum StockAcquiredSubscriber {
-	INSTANCE;
+  INSTANCE;
 
-	private final Logger logger = Logger.basicLogger();
+  private final Logger logger = Logger.basicLogger();
 
-	private static final String EXCHANGE_NAME = "logs"; // ???
+  private static final String EXCHANGE_NAME = "logs"; // ???
 
-	StockAcquiredSubscriber() {
-		final ConnectionFactory factory = new ConnectionFactory();
-		factory.setHost("localhost");
+  StockAcquiredSubscriber() {
+    final ConnectionFactory factory = new ConnectionFactory();
+    factory.setHost("localhost");
 
-		try (final Connection connection = factory.newConnection();
-				final Channel channel = connection.createChannel()) {
+    try (final Connection connection = factory.newConnection();
+        final Channel channel = connection.createChannel()) {
 
-			channel.exchangeDeclare(EXCHANGE_NAME, "fanout");
-			String queueName = channel.queueDeclare().getQueue();
-			channel.queueBind(queueName, EXCHANGE_NAME, "");
+      channel.exchangeDeclare(EXCHANGE_NAME, "fanout");
+      String queueName = channel.queueDeclare().getQueue();
+      channel.queueBind(queueName, EXCHANGE_NAME, "");
 
-			logger.debug("Waiting for messages..");
+      logger.debug("Waiting for messages..");
 
-			DeliverCallback deliverCallback = (consumerTag, delivery) -> {
-				String message = new String(delivery.getBody(), StandardCharsets.UTF_8);
-				logger.debug("Received '" + message + "' with a length of: " + message.length());
-			};
-			String test = channel.basicConsume(queueName, true, deliverCallback, consumerTag -> {
-			});
+      DeliverCallback deliverCallback =
+          (consumerTag, delivery) -> {
+            String message = new String(delivery.getBody(), StandardCharsets.UTF_8);
+            logger.debug("Received '" + message + "' with a length of: " + message.length());
+          };
+      String test = channel.basicConsume(queueName, true, deliverCallback, consumerTag -> {});
 
-			logger.debug(test);
-		} catch (IOException | TimeoutException e) {
-			logger.debug(e.getMessage(), e);
-		}
-	}
-
+      logger.debug(test);
+    } catch (IOException | TimeoutException e) {
+      logger.debug(e.getMessage(), e);
+    }
+  }
 }
