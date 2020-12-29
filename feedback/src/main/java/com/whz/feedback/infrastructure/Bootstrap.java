e8dbcd7bad1b8b4f0aab374ba2e527361705885c
flaxel <falk.puschner@arcor.de>
2020-12-29 13:59:42 +0100

    :memo: add bootstrap javadoc

--- feedback/src/main/java/com/whz/feedback/infrastructure/Bootstrap.java
+++ feedback/src/main/java/com/whz/feedback/infrastructure/Bootstrap.java
@@ -21,13 +21,21 @@ import io.vlingo.http.resource.Server;
 import io.vlingo.lattice.model.sourcing.SourcedTypeRegistry;
 import io.vlingo.lattice.model.stateful.StatefulTypeRegistry;
 
+/**
+ * This class represents the main class for the entire feedback application. It can be used to start
+ * the program.
+ *
+ * @since 1.0.0
+ */
 public class Bootstrap {
 
-  private static final Logger logger = Logger.basicLogger();
-
+  /** standard port for the application */
   private static final int DEFAULT_PORT = 18080;
 
+  /** name of the application */
   private static final String NAME = "feedback";
+
+  private static final Logger logger = Logger.basicLogger();
 
   private static Bootstrap instance;
 
@@ -35,6 +43,13 @@ public class Bootstrap {
 
   private final World world;
 
+  /**
+   * Initialize the program and start the server.
+   *
+   * @param port port to reach the application
+   * @throws Exception if something went wrong
+   * @since 1.0.0
+   */
   public Bootstrap(final int port) throws Exception {
     world = World.startWithDefaults(NAME);
 
@@ -60,12 +75,18 @@ public class Bootstrap {
 
                     logger.info("\n");
                     logger.info("=========================");
-                    logger.info("Stopping feedback.");
+                    logger.info("Stopping {}.", NAME);
                     logger.info("=========================");
                   }
                 }));
   }
 
+  /**
+   * Shutting down the server and releasing occupied resources.
+   *
+   * @throws Exception if the instance could not be created
+   * @since 1.0.0
+   */
   void stopServer() throws Exception {
     if (instance == null) {
       throw new IllegalStateException("Schemata server not running");
@@ -79,7 +100,7 @@ public class Bootstrap {
 
   public static void main(final String[] args) throws Exception {
     logger.info("=========================");
-    logger.info("service: feedback.");
+    logger.info("service: {}.", NAME);
     logger.info("=========================");
 
     int port;
@@ -88,7 +109,7 @@ public class Bootstrap {
       port = Integer.parseInt(args[0]);
     } catch (Exception e) {
       port = DEFAULT_PORT;
-      logger.warn("feedback: Command line does not provide a valid port; defaulting to: {}", port);
+      logger.warn("{}: Command line does not provide a valid port; defaulting to: {}", NAME, port);
     }
 
     instance = new Bootstrap(port);
