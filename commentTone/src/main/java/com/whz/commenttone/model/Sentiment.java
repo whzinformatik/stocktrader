3d308c32291d34724f65d498ca1fbb497bb56693
kb1prb13 <dastan.namazov.96@gmail.com>
2021-01-13 15:29:32 +0100

    Execute spotless apply

--- commentTone/src/main/java/com/whz/commenttone/model/Sentiment.java
+++ commentTone/src/main/java/com/whz/commenttone/model/Sentiment.java
@@ -13,7 +13,9 @@ package com.whz.commenttone.model;
  *
  * @since 1.0.0
  */
-
 public enum Sentiment {
-    UNKNOWN, POSITIVE, NEUTRAL, NEGATIVE
+  UNKNOWN,
+  POSITIVE,
+  NEUTRAL,
+  NEGATIVE
 }
