32641fe1ca65fed17f2266cd3a77fbe18424b41d
flaxel <falk.puschner@arcor.de>
2021-01-14 15:34:14 +0100

    :memo: update inception year

--- portfolio/src/main/java/com/whz/portfolio/model/portfolio/PortfolioState.java
+++ portfolio/src/main/java/com/whz/portfolio/model/portfolio/PortfolioState.java
@@ -1,5 +1,5 @@
 /*
- * Copyright © 2020, Fachgruppe Informatik WHZ <help.flaxel@gmail.com>
+ * Copyright © 2020-2021, Fachgruppe Informatik WHZ <help.flaxel@gmail.com>
  *
  * This Source Code Form is subject to the terms of the Mozilla Public
  * License, v. 2.0. If a copy of the MPL was not distributed with this
