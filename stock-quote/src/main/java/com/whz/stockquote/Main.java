3bb2d1f2c02d1db0323550b55c09c2d93cb1f49a
Simon <48348803+simeck26@users.noreply.github.com>
2021-01-15 17:31:23 +0100

    Stock quote documentation (#48)
    * Add license
    
    * Updated year
    
    * Apply new license header
    
    * Refactor code and add documentation
    
    * Add durability and apply spotless
    
    * Update environment variables
    
    * Update README.md
    
    * Change logger
    
    * Change restart policy
    
    * Update connection and channel management
    
    * Add custom exception for failed stock requests
    
    * Add documnetation and apply spotless
    
    * Code refactoring
    
    * Change restart policy of RabbitMQ
    
    * Update documentation
    
    * Update handling of incorrect symbols and update documentation

--- stock-quote/src/main/java/com/whz/stockquote/Main.java
+++ stock-quote/src/main/java/com/whz/stockquote/Main.java
@@ -1,24 +1,105 @@
+/*
+ * Copyright © 2020-2021, Fachgruppe Informatik WHZ <help.flaxel@gmail.com>
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
 package com.whz.stockquote;
 
-import java.io.IOException;
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.Optional;
+import org.apache.log4j.BasicConfigurator;
 
+/**
+ * This class represents the main class for the stock quote application. It can be used to start the
+ * program.
+ *
+ * @since 1.0.0
+ */
 public class Main {
 
-  public static void main(String[] args) throws IOException, InterruptedException {
-    String serviceName = System.getenv("RABBITMQ_SERVICE");
-    String exchangeName = System.getenv("RABBITMQ_EXCHANGE");
-    String exchangeType = System.getenv("RABBITMQ_EXCHANGE_TYPE");
+  /**
+   * Name of the RabbitMQ service.
+   *
+   * @since 1.0.0
+   */
+  private final String serviceName;
 
-    List<String> symbols =
-        Arrays.asList(
-            "TSLA", "INTC", "GOOG", "MSFT", "AMZN", "NVDA", "NFLX", "SBUX", "SAP", "ADDYY", "POAHF",
-            "SAP.DE");
+  /**
+   * Name of the RabbitMQ exchange.
+   *
+   * @since 1.0.0
+   */
+  private final String exchangeName;
 
+  /**
+   * Durability of the RabbitMQ exchange.
+   *
+   * @since 1.0.0
+   */
+  private final boolean durableExchange;
+
+  /**
+   * Time interval between each message publication (in seconds).
+   *
+   * @since 1.0.0
+   */
+  private int publishInterval;
+
+  /**
+   * Randomness of the {@code publishInterval}.
+   *
+   * @since 1.0.0
+   */
+  private boolean publishRandomly;
+
+  /**
+   * List of stock symbols.
+   *
+   * @since 1.0.0
+   */
+  private final List<String> symbols;
+
+  /**
+   * Create an object which contains information for the publisher setup.
+   *
+   * @since 1.0.0
+   */
+  public Main() {
+    this.serviceName = Optional.ofNullable(System.getenv("RABBITMQ_SERVICE")).orElse("localhost");
+    this.exchangeName = Optional.ofNullable(System.getenv("RABBITMQ_EXCHANGE")).orElse("stocks");
+
+    String durableExchangeString =
+        Optional.ofNullable(System.getenv("DURABLE_EXCHANGE")).orElse("false");
+    this.durableExchange = Boolean.parseBoolean(durableExchangeString);
+
+    String publishIntervalString =
+        Optional.ofNullable(System.getenv("PUBLISH_INTERVAL")).orElse("3");
+    this.publishInterval = Integer.parseInt(publishIntervalString);
+
+    String randomPublishIntervalString =
+        Optional.ofNullable(System.getenv("RANDOM_PUBLISH_INTERVAL")).orElse("0");
+    if (!randomPublishIntervalString.equals("0")) {
+      this.publishInterval = Integer.parseInt(randomPublishIntervalString);
+      this.publishRandomly = true;
+    }
+
+    String symbolsString =
+        Optional.ofNullable(System.getenv("STOCK_SYMBOLS"))
+            .orElse("TSLA,INTC,GOOG,MSFT,AMZN,NVDA,NFLX,SBUX,SAP,ADDYY,POAHF,SAP.DE");
+    this.symbols = new ArrayList<>(Arrays.asList(symbolsString.toUpperCase().split("\\s*,\\s*")));
+  }
+
+  public static void main(String[] args) {
+    BasicConfigurator.configure();
+
+    Main m = new Main();
     StockQuotePublisher stockQuotePublisher =
-        new StockQuotePublisher(serviceName, exchangeName, exchangeType);
+        new StockQuotePublisher(m.serviceName, m.exchangeName, m.durableExchange);
 
-    stockQuotePublisher.run(symbols);
+    stockQuotePublisher.run(m.symbols, m.publishInterval, m.publishRandomly);
   }
 }
--- stock-quote/src/main/java/com/whz/stockquote/StockQuotePublisher.java
+++ stock-quote/src/main/java/com/whz/stockquote/StockQuotePublisher.java
@@ -1,40 +1,96 @@
+/*
+ * Copyright © 2020-2021, Fachgruppe Informatik WHZ <help.flaxel@gmail.com>
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
 package com.whz.stockquote;
 
+import com.rabbitmq.client.BuiltinExchangeType;
 import com.rabbitmq.client.Channel;
 import com.rabbitmq.client.Connection;
 import com.rabbitmq.client.ConnectionFactory;
 import java.io.IOException;
 import java.nio.charset.StandardCharsets;
+import java.util.Iterator;
 import java.util.List;
 import java.util.concurrent.TimeoutException;
-import java.util.logging.Logger;
 import org.json.JSONObject;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import yahoofinance.Stock;
 import yahoofinance.YahooFinance;
 
-public class StockQuotePublisher {
+/**
+ * This class can be used to publish stock quote data to RabbitMQ. The stock quotes are obtained
+ * from the "Finance Quotes API for Yahoo Finance" by Stijn Strickx.
+ *
+ * @see <a href="https://github.com/sstrickx/yahoofinance-api">yahoofinance-api by Stijn Strickx</a>
+ * @since 1.0.0
+ */
+class StockQuotePublisher {
 
-  private static final Logger LOGGER = Logger.getLogger(StockQuotePublisher.class.getName());
+  private static final Logger logger = LoggerFactory.getLogger(StockQuotePublisher.class);
 
   private final String exchangeName;
-  private final String exchangeType;
+  private final boolean durableExchange;
 
   private final ConnectionFactory connectionFactory;
 
-  public StockQuotePublisher(String serviceName, String exchangeName, String exchangeType) {
+  /**
+   * Create a publisher which is connected to a specific RabbitMQ instance.
+   *
+   * @param serviceName name of RabbitMQ host
+   * @param exchangeName name of RabbitMQ exchange
+   * @param durableExchange true if RabbitMQ exchange should be durable, false otherwise
+   * @since 1.0.0
+   */
+  public StockQuotePublisher(String serviceName, String exchangeName, boolean durableExchange) {
     this.exchangeName = exchangeName;
-    this.exchangeType = exchangeType;
+    this.durableExchange = durableExchange;
 
     this.connectionFactory = new ConnectionFactory();
     this.connectionFactory.setHost(serviceName);
   }
 
-  public void publish(List<String> symbols) throws IOException {
-    try (Connection connection = connectionFactory.newConnection();
-        Channel channel = connection.createChannel()) {
-      channel.exchangeDeclare(exchangeName, exchangeType);
-      for (String s : symbols) {
+  /**
+   * Retrieve stock information and publish a new message to the RabbitMQ instance. The message
+   * contains a string in JSON format with following information of a stock:
+   *
+   * <ul>
+   *   <li>symbol
+   *   <li>name
+   *   <li>price
+   *   <li>currency
+   *   <li>time
+   * </ul>
+   *
+   * @param symbols list of official stock symbols
+   * @param channel RabbitMQ channel which messages will get published into
+   * @see <a href="https://www.nasdaq.com/market-activity/stocks/screener">Nasdaq website with list
+   *     of official stocks</a>
+   * @since 1.0.0
+   */
+  private void publish(List<String> symbols, Channel channel) {
+    try {
+      channel.exchangeDeclare(exchangeName, BuiltinExchangeType.FANOUT, durableExchange);
+      Iterator<String> itr = symbols.iterator();
+      while (itr.hasNext()) {
+        String s = itr.next();
         Stock answer = YahooFinance.get(s);
+        try {
+          if (answer == null) {
+            itr.remove();
+            throw new StockNotFoundException(
+                String.format(
+                    "Could not find stock for symbol '%s'! Symbol will be removed from symbol list!",
+                    s));
+          }
+        } catch (StockNotFoundException e) {
+          logger.warn(e.toString(), e);
+          continue;
+        }
         String answerJsonString =
             new JSONObject()
                 .put("symbol", answer.getSymbol())
@@ -46,26 +102,56 @@ public class StockQuotePublisher {
         channel.basicPublish(
             exchangeName, "", null, answerJsonString.getBytes(StandardCharsets.UTF_8));
       }
-    } catch (TimeoutException e) {
-      LOGGER.severe(e.toString());
+    } catch (IOException e) {
+      logger.error("Could not retrieve stock information!", e);
     }
   }
 
-  public void run(List<String> symbols) throws IOException, InterruptedException {
-    String randomPublishIntervalString = System.getenv("SET_RANDOM_PUBLISH_INTERVAL");
+  /**
+   * Creates a connection and a channel to publish messages to RabbitMQ. Publish messages to
+   * RabbitMQ continuously. The publishing interval may be a random number within zero and the
+   * {@code publishInterval} as an upper limit.
+   *
+   * @param symbols list of official stock symbols
+   * @param publishInterval time interval in seconds
+   * @param publishRandomly true if publishing interval should be random, false otherwise
+   * @since 1.0.0
+   */
+  public void run(List<String> symbols, int publishInterval, boolean publishRandomly) {
+    try {
+      Connection connection = connectionFactory.newConnection();
+      Channel channel = connection.createChannel();
 
-    if (Boolean.parseBoolean(randomPublishIntervalString)) {
+      int actualPublishInterval = publishInterval * 1000;
+
       while (true) {
-        publish(symbols);
-        Thread.sleep((long) (Math.random() * 3000));
+        publish(symbols, channel);
+
+        if (publishRandomly) {
+          actualPublishInterval = (int) (Math.random() * publishInterval * 1000);
+        }
+        logger.info(String.format("Publishing interval: %dms", actualPublishInterval));
+
+        try {
+          Thread.sleep(actualPublishInterval);
+        } catch (InterruptedException e) {
+          logger.error("Thread got interrupted!", e);
+        }
       }
-    } else {
-      LOGGER.warning(
-          "Environment variable 'SET_RANDOM_PUBLISH_INTERVAL' either incorrect or not set!");
-      while (true) {
-        publish(symbols);
-        Thread.sleep(30000);
-      }
+    } catch (IOException | TimeoutException e) {
+      logger.error(
+          "Connection error! Check if your RabbitMQ service is running and accessible!", e);
+    }
+  }
+
+  /**
+   * Exception which is thrown if a stock could not get retrieved.
+   *
+   * @since 1.0.0
+   */
+  private static class StockNotFoundException extends Exception {
+    public StockNotFoundException(String message) {
+      super(message);
     }
   }
 }
